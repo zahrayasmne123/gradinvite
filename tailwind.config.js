96c0d7a7ac5f9fe5ecdf9f34254964af3d3cfbd9
zahrayasmne123 <132264334+zahrayasmne123@users.noreply.github.com>
2026-01-02 11:02:43 +0000

    fix: update css to use tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
   plugins: [],
 }
-
